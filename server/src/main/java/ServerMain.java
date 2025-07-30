e84788220c2a6f1735d51e75e2c4a6b1334141a7
Mathias Alejandro Gallo Bedon <alejandrogallo@Mathiass-MacBook-Air.local>
2025-07-30 13:13:56 -0600

    getting started code for phase 5

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,8 +1,7 @@
 import chess.*;
-import dataaccess.DataAccessException;
 import server.Server;
-import dataaccess.DatabaseManager;
-public class Main {
+
+public class ServerMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
